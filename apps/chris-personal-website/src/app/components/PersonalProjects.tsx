f87d7a2b19a389cb17f42d417d37249ec870ca8a
Chris May <r16cmay@gmail.com>
2021-08-14 12:05:36 -0700

    grid setup pt1

--- apps/chris-personal-website/src/app/components/PersonalProjects.tsx
+++ apps/chris-personal-website/src/app/components/PersonalProjects.tsx
@@ -1,10 +1,32 @@
-import { Heading } from "../common/styles/Heading"
-import { Section } from "../common/styles/Section"
+import { GridContainer } from '../common/styles/GridContainer';
+import { Heading } from '../common/styles/Heading';
+import { Section } from '../common/styles/Section';
+import { Card } from '../common/components/Card';
 
 export const PersonalProjects = () => {
-    return (
-      <Section className='personal' direction="row">
-          <Heading primary={true}>Personal Projects</Heading>
-      </Section>
-    )
-}
+  return (
+    <Section className="personal" direction="column">
+      <Heading primary={true}>Personal Projects</Heading>
+      <GridContainer>
+        <Card>
+          <h3>Project 1</h3>
+        </Card>
+        <Card>
+          <h3>Project 2</h3>
+        </Card>
+        <Card>
+          <h3>Project 3</h3>
+        </Card>
+        <Card>
+          <h3>Project 4</h3>
+        </Card>
+        <Card>
+          <h3>Project 5</h3>
+        </Card>
+        <Card>
+          <h3>Project 6</h3>
+        </Card>
+      </GridContainer>
+    </Section>
+  );
+};
