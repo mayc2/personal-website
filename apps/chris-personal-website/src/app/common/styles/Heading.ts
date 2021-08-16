e8e7c65a41f846bfe6e024430e63148a4e38a86c
Chris May <r16cmay@gmail.com>
2021-08-15 18:23:57 -0700

    about section content update and Header styling updates

--- apps/chris-personal-website/src/app/common/styles/Heading.ts
+++ apps/chris-personal-website/src/app/common/styles/Heading.ts
@@ -1,19 +1,23 @@
-import styled from "@emotion/styled";
-import { PRIMARY_FONT_COLOR, SECONDARY_FONT_COLOR } from "../constants/colors";
+import styled from '@emotion/styled';
+import { PRIMARY_FONT_COLOR, SECONDARY_FONT_COLOR } from '../constants/colors';
 
 type HeadingProps = {
   primary?: boolean;
-}
+  margin?: string;
+  paddingBottom?: string;
+};
 
 export const Heading = styled.h1`
   font-size: 3rem;
-  padding-bottom: 3rem;
-  margin: 50px;
+  padding-bottom: ${(props: HeadingProps) =>
+    props.paddingBottom ? props.paddingBottom : '3rem'};
+  margin: ${(props: HeadingProps) => (props.margin ? props.margin : '50px')};
   text-align: center;
   color: ${(props: HeadingProps) =>
     props.primary ? PRIMARY_FONT_COLOR : SECONDARY_FONT_COLOR};
 
   @media screen and (max-width: 768px) {
     font-size: 2rem;
+    margin: 20px;
   }
 `;
